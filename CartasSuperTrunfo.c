d3abd12fad104a31f3f3a65b7a7b206cecd5c82b
ClAlexandreMattos <alexancler2004@hotmail.com>
2025-02-21 23:59:43 +0000

    codcarta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,14 +9,18 @@ int main(){
     printf("Carta 1:\n");
 
          char estado[1];
+         char codcarta[3]; //código da carta
 
             printf("Digite o  Estado: \n");
             scanf("%s", estado);
 
+            printf("Digite o Código: \n");
+            scanf("%s", codcarta)
 
 
             printf("Carta 1:\n");
             printf("Estado:%s\n",estado);
+            printf("Código:%s\n",codcarta");
 
 
 
