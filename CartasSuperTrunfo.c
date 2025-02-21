2a8268e00d405fd91e83b55fbe5f3269a1859116
ClAlexandreMattos <alexancler2004@hotmail.com>
2025-02-21 23:44:15 +0000

    Cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,37 +4,21 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
-int main() {
-    char estado[20];
-    char codigo da carta[3];
-    char nome da cidade[20];
-    int populacao;
-    float pib;
-    int numero de pontos turisticos;
+int main(){
     
-    printf("Desafio Cartas Super Trunfo \n");
-        printf("Carta 1:\n");
-            printf("Digite um estado: \n");
-            scanf("%s", &estado);
 
-            printf("Digite o codigo da carta: \n");
-            scanf("%s", &codigo da carta);
+         char estado[1];
 
-            printf("Digite o nome da cidade: \n");
-            scanf("%s", &nome da cidade);
+            printf("Digite o  Estado: \n");
+            scanf("%s", estado);
 
-            printf("Digite a populacao: \n");
-            scanf("%f", &População);
-
-            printf("Digite o pib: \n");
-            scanf("%f", &PIB);
-
-            printf("Digite o numero de pontos turisticos: \n");
-            scanf("%d", &Numero de Pontos Turísticos);
+            printf("Carta 1:\n");
+            printf("Estado:%s\n",estado);
 
 
-            return 0;
-        }
-    
+
+    return 0;
+
+
+}
