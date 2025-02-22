a6e309bf07e54dab41a58dd52001eaa34a20a69c
ClAlexandreMattos <alexancler2004@hotmail.com>
2025-02-22 01:14:34 +0000

    população laçada

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,25 +8,30 @@
 int main(){
     printf("Carta 1:\n");
 
-         char estado[1];
-         char codcarta[3]; //código da carta
-         char nome[20]; //nome da cidade
+         char estado[20];
+         char codcarta[40]; //código da carta
+         char nome[30]; //nome da cidade
+         int populacao; // total de habitante
 
 
             printf("Digite o Estado: \n");
             scanf("%s", estado);
 
             printf("Digite o Código da Carta: \n");
-            scanf("%s",codcarta);
+            scanf("%s", codcarta);
 
             printf("Digite o Nome da Cidade: \n");
             scanf("%s", nome);
 
+            printf("Digite a População: \n");
+            scanf("%d", populacao);
+
 
             printf("Carta 1:\n");
-            printf("Estado:%s\n",estado);
+            printf("Estado:%s\n", estado);
             printf("Código:%s\n", codcarta);
             printf("Nome da Cidade:%s\n", nome);
+            printf("População:%s\n", populacao);
 
             
     return 0;
